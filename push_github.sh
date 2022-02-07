4e4981d53b064e3c8b914c60a99aae950f4b81d9
DanielTrieu <trieuvietcuong@gmail.com>
2022-02-07 19:45:46 +0800

    update from vmware

--- push_github.sh
+++ push_github.sh
@@ -1,6 +1,7 @@
-#cd /home/cuongtv/strapi/
+#!/bin/sh
+cd /home/daniel/A2A_MAS
 git add -A
 git commit -a -m "update from vmware"
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/strapi_ssh
+ssh-add home/daniel/.ssh/strapi_ssh
 git push origin main
