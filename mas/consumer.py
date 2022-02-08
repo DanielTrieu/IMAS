d6f462efb7104e3f760a9811f7980ee55c97c648
DanielTrieu <trieuvietcuong@gmail.com>
2022-02-08 16:15:01 +0800

    update from vmware

--- mas/consumer.py
+++ mas/consumer.py
@@ -111,50 +111,7 @@ class ConsumerAgent(Agent):
                 print("no matching Bot handle")
 
      
-    class Handler(CyclicBehaviour):
-        async def run(self):
-           
-            msg_recv = await self.receive(100000) # wait for a message for 10 seconds
-      
-            if msg_recv.body:
-     
-                print("Message received with content:", msg_recv.body)
-                message = msg_recv.body
-                sender = str(msg_recv.sender)
-                print("senderid", sender)
-                rasa_responses = await rasa_agent.handle_text(message, sender_id=sender)
-                if rasa_responses:
-                    
-                    # rasa_responses : [{'recipient_id': 'default', 'text': 'All done!'}, {'recipient_id': 'default', 'custom': {'product': 'carrot', 'quantity': '20', 'price': '35', 'self_made_product': 'True'}}]
-                    print(rasa_responses)
-                    for response in rasa_responses:
-                        for msg_type, msg_content in response.items():
-                            if msg_type == "text":
-                                #sender = str(msg_recv.sender)
-                                msg_sent =Message(to=sender)
-                                msg_sent.body= msg_content
-                                await self.send(msg_sent)
-                            if msg_type =="custom":
-                                print (msg_content)
-                                self.set("buy_data",msg_content)
-                                await self.agent.run_CFP()
-                                msg_sent =Message(to=sender)
-                                msg_sent.body= "Finish set up " + str(msg_content)
-                                await self.send(msg_sent)    
-
-
-            
-            if msg_recv.get_metadata('performative')=="propose":
-                
-                self.set("seller", str(msg_recv.sender))
-                await self.agent.run_accept_proposal()
-
-            if msg_recv.get_metadata('performative')=="confirm":
-                to_agent ="daniel@talk.tcoop.org"
-                msg = Message(to=to_agent)
-                msg.body = "receive confirm from "+((str(msg_recv.sender)).split("/"))[0]+" : "+str(msg_recv.metadata)
-                await self.send(msg)
-                
+                  
 
 
     class CFP(OneShotBehaviour):
