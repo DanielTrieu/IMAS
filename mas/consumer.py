04b8f8fba3de3df8209c7459e60dd5dce93daf02
DanielTrieu <trieuvietcuong@gmail.com>
2022-02-08 16:14:01 +0800

    update from vmware

--- mas/consumer.py
+++ mas/consumer.py
@@ -10,6 +10,106 @@ rasa_agent = rasaAgent.create_agent('/home/daniel/rasa/test/models/20211107-2030
 
 
 class ConsumerAgent(Agent):
+
+
+     async def userhandle(self,user_inform):
+
+            if user_inform["inform"]=="supply":
+                self.set("supply_data", user_inform)
+                self.set ("to_agent",["market@talk.tcoop.org"])
+                print(user_inform)
+                
+                self.add_behaviour(self.Propose())
+
+    
+    class UserHandler(CyclicBehaviour):
+                
+        async def rasabot(message):
+        
+            reply ="sorry no reply"
+            reply_type ="text"
+        
+            rasa_responses = await rasa_agent.handle_text(message, sender_id=sender)
+            if rasa_responses:
+                
+            # rasa_responses : [{'recipient_id': 'default', 'text': 'All done!'}, {'recipient_id': 'default', 'custom': {'product': 'carrot', 'quantity': '20', 'price': '35', 'self_made_product': 'True'}}]
+                print(rasa_responses)
+                for response in rasa_responses:
+                    for msg_type, msg_content in response.items():
+                        if msg_type == "text":
+                            #sender = str(msg_recv.sender)
+                            msg_sent =Message(to=sender)
+                            msg_sent.body= msg_content
+                            await self.send(msg_sent)
+                        if msg_type =="custom":
+                            print (msg_content)
+                            
+                            reply_type = msg_type
+                            reply = msg_content
+
+            else: await self.send("sorry no reply")
+
+            return reply_type, reply 
+
+              
+        
+        async def run(self):
+            
+            msg_recv = await self.receive(1000000) # wait for a message for 10 seconds
+
+            print()
+            print("Receive message user", msg_recv)
+            print()
+            # message from rasa
+
+            if msg_recv.body:        
+                print("Message received with content:", msg_recv.body)
+                message = msg_recv.body
+                sender = str(msg_recv.sender)
+                print("senderid", sender)
+
+                reply_type, reply = await rasabot(message)
+                """
+                receive customer data from rasa
+                """
+                if reply_type =="custom":
+                    await userhandle(reply)
+                    print(reply)
+
+
+            elif msg_recv.get_metadata('performative')=="user_inform":
+                user_inform = copy.deepcopy(msg_recv.metadata)
+                self.agent.set('sup')
+                user_inform['performative']="inform"
+                print(user_inform)
+                await self.agent.userhandle(user_inform)
+                
+      
+
+    class BotHandler(CyclicBehaviour):
+        async def run(self):
+            
+            msg_recv = await self.receive(1000000) # wait for a message for 10 seconds
+            
+            print()
+            print("Receive message bot handle", msg_recv)
+            print()
+            
+            if msg_recv.get_metadata('performative')=="cfp":
+                time.sleep(1)
+                await self.agent.run_propose()
+            elif msg_recv.get_metadata('performative')== "accept-proposal":
+                time.sleep(1)
+                await self.agent.run_confirm()
+                sell_data = self.get('sell_data')
+                new_quantity =  int(sell_data['quantity']) - int(msg_recv.metadata['quantity'])
+                sell_data["quantity"]= str(new_quantity )
+                self.set('sell_data',sell_data)
+                print ('new quantity', sell_data['quantity'])
+
+            else:
+                print("no matching Bot handle")
+
      
     class Handler(CyclicBehaviour):
         async def run(self):
